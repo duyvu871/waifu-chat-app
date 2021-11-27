232407839a96c83d73611a8d4b83c88a733c8d85
duyvu871 <78553549+duyvu871@users.noreply.github.com>
2021-11-27 17:31:27 +0700

    update v2
    fix save data and add enter handle

--- app/app.js
+++ app/app.js
@@ -4,26 +4,44 @@ import {setBubbleChat,$,$$,createElement,getParent,setEvent,getUser,setStyle,DEL
 import {isEqual} from './index.js';
 (($,$$,createElement,getParent)=>{
     $('.middle .waiting').remove()
-    let cookies = cookie()
+    let cookies = cookie();
     //
-    let userLogin = cookies.read('userLogin') ? cookies.decode(cookies.read('userLogin')) : undefined
+    let userLogin = cookies.read('userLogin') 
+                            ? cookies.decode(cookies.read('userLogin')) 
+                            : undefined;
     //
-    let userChat = userLogin ? userLogin.chat : undefined
+    let userChat = userLogin ? userLogin.chat : undefined;
     //
-    let userStatus = userLogin ? userLogin.status : undefined
+    let userStatus = userLogin ? userLogin.status : undefined;
     //
-    let colorCode = ['#79c7c5','#fff','#e283d6','#0084ff']
-    let setColorPrimary = color => $(':root').style.setProperty('--primary-color',color) 
-    let primaryColor = userLogin ? userLogin.color : colorCode[0]
+    let colorCode = ['#79c7c5','#fff','#e283d6','#0084ff'];
+    let setColorPrimary = color => $(':root').style.setProperty('--primary-color',color);
+    let primaryColor = userLogin ? userLogin.color : colorCode[0];
     //
     let shadowData = {
         login:userLogin ? userLogin.login : undefined ,
         color:primaryColor,
         status:userStatus,
         chat:userChat
-    }
-    // console.log(userLogin);
+    };
     //
+    function pickMessage () {
+        let postStatus = true ;
+        let inputValue = document.querySelector('.chat input').value;
+        if(inputValue != '' && inputValue.toLowerCase().trim() !== 'logout') {
+            setBubbleChat(inputValue,'post')
+            if (postStatus) {
+                $('.chat input').value = ''
+                sendMessage(inputValue)
+            }
+        }else if(inputValue.toLowerCase().trim() === 'logout') {
+            //
+            shadowData.status = "off";
+            shadowData.chat = getUser().message;
+            location.reload();
+            
+        }
+    }
     if (userLogin && userStatus === 'on') {
         [...$('.login-html').children].forEach(v=>{ v.style.display = "none" })
         $('.user').style.display = 'flex' 
@@ -42,34 +60,35 @@ import {isEqual} from './index.js';
 
         userChat.forEach(element=>setBubbleChat(element.message, element.status));
         [...$$('input[name="radio1"]')].forEach(element=> {
-            let color = getParent(element,'.b-contain').querySelector('span').textContent
+            let color = getParent(element,'.b-contain').querySelector('span').textContent;
             if (color === userLogin.color) element.checked = true
         })
     } else {
         setEvent('.group .signin',true,'click',(e)=>{
-            let checked = []
-            let userInfo = getUser().login
+            let checked = [];
+            let userInfo = getUser().login;
             
             for (const key in userInfo) {
                 //
                 if (userInfo[key] == '' ) checked.concat([userInfo[key]])
             }
+
             if (!checked.includes('')) {
                 //
                 let checkDuplicate = userLogin 
                                     && userStatus === 'off' 
                                     && isEqual(userInfo,userLogin.login)
-                                    && shadowData.color === userLogin.color
-                                
+                                    && shadowData.color === userLogin.color;
+     
                 if(checkDuplicate) {
                     shadowData.status = 'on'
                   // tại đây nếu như user đăng nhập lại tài khoản đã đăng suất thì sẽ quay lại
                 }else if(!checkDuplicate && getUser().message.length === 0){
                     //
-                    shadowData.login = userInfo
-                    shadowData.color = primaryColor
-                    shadowData.status = 'on'
-                    shadowData.chat = getUser().message
+                    shadowData.login = userInfo;
+                    shadowData.color = primaryColor;
+                    shadowData.status = 'on';
+                    shadowData.chat = getUser().message;
                 }
                 setTimeout(()=>{ location.reload() },DELAY_RELOAD)
             }
@@ -78,41 +97,39 @@ import {isEqual} from './index.js';
 
     setColorPrimary(primaryColor);
 
-    setEvent('.chat button',true,'click',(e)=>{
-        let postStatus = true
-        let inputValue = getParent(e.target,'.chat').querySelector('input').value
-        if(inputValue != '' && inputValue.toLowerCase().trim() !== 'logout') {
-            setBubbleChat(inputValue,'post')
-            if (postStatus) {
-                $('.chat input').value = ''
-                sendMessage(inputValue)
-            }
-        }else if(inputValue.toLowerCase().trim() === 'logout') {
-            //
-            shadowData.status = "off"
-            shadowData.chat = getUser().message
-            location.reload()
-            
+    setEvent('.chat button',true,'click',pickMessage);
+    setEvent('.chat input',true,'keypress',(e)=>{
+        if(e.which === 13){
+            pickMessage()
         }
-    });
-
+    })
     setEvent('input[name="radio1"]',false,'input',(event)=>{
-        let color = getParent(event.target,'.b-contain').querySelector('span').textContent
+        let color = getParent(event.target,'.b-contain')
+                            .querySelector('span')
+                            .textContent;
         if(event.target.checked){
             setColorPrimary(color)
-            primaryColor = color
+            primaryColor = color;
         }
     });
 
     window.addEventListener("beforeunload", (event) => {
         //
-            let message = getUser().message
+
+            let message = getUser().message;
+
+
+
             let data = {
                 login:shadowData.login,
                 color:shadowData.color,
                 status:shadowData.status,
-                chat: message.length > 0 ? message : shadowData.chat,
-            }
+
+                chat:message.length > 0 ? message : shadowData.chat,
+            };
+            console.log(data.chat);
+
+
             cookies.create('userLogin',JSON.stringify(data),Date.UTC(2024,12,2))
         // event.returnValue = "There are unsaved changes. Leave now?";
       });
