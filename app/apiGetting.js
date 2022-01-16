661ca640ccaf98d1b400664baf58f55a9bc67245
duyvu871 <78553549+duyvu871@users.noreply.github.com>
2022-01-16 14:30:45 +0700

    new update fix error new api

--- app/apiGetting.js
+++ app/apiGetting.js
@@ -1,13 +1,32 @@
 import {setBubbleChat} from './global.js'
 function sendMessage (message){
+    var headerParams = {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        "expires": "0",
+        "pragma": "no-cache",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "sec-gpc": "1",
+        "usequerystring": "true",
+        "x-rapidapi-host": "waifu.p.rapidapi.com",
+        "x-rapidapi-key": "9d2cfc39c7msh3f991e24c5ae59cp1d1a91jsn16e4ec494996",
+        "x-rapidapi-ua": "RapidAPI-Playground"
+    }
+
     var myHeaders = new Headers();
-    myHeaders.append("content-type", "application/x-www-form-urlencoded");
-    myHeaders.append("x-rapidapi-host", "https://waifu.p.rapidapi.com");
-    myHeaders.append("x-rapidapi-key", "9d2cfc39c7msh3f991e24c5ae59cp1d1a91jsn16e4ec494996");
+    
+    for (var index in headerParams) {
+        myHeaders.append(index,headerParams[index])
+    }
+
 
     var urlencoded = new URLSearchParams();
     urlencoded.append("message", message);
-    urlencoded.append("user_id", "9fa832g79fh2398h");
+    urlencoded.append("user_id", "sample_user_id");
     urlencoded.append("from_name", "hinari");
     urlencoded.append("to_name", "shinobu");
     urlencoded.append("situation", "Boy loves Girl.");
